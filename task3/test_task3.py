cc3a979dfb4d08347d607e9ca95f37b373281f66
Mark Avilin <46706090+MarkAvilin1@users.noreply.github.com>
2023-04-28 04:47:28 +0800

    Update test_task3

--- task3/test_task3.py
+++ task3/test_task3.py
@@ -9,7 +9,16 @@ def test_combine_sentences_simple():
 
     combined_sentence = combine_sentences(sentence1, sentence2)
 
-    assert combined_sentence == "I like pizza. Pizza is my favorite food."
+    assert combined_sentence == "I like pizza, and pizza is my favorite food."
+
+
+def test_combine_sentences_with_common_words():
+    sentence1 = "The book was on the table."
+    sentence2 = "The table was made of wood."
+
+    combined_sentence = combine_sentences(sentence1, sentence2)
+
+    assert combined_sentence == "The book was on the table, and the table was made of wood."
 
 
 def test_combine_sentences_with_conjunction():
@@ -18,7 +27,7 @@ def test_combine_sentences_with_conjunction():
 
     combined_sentence = combine_sentences(sentence1, sentence2)
 
-    assert combined_sentence == "I went to the store. I forgot my wallet."
+    assert combined_sentence == "I went to the store, but I forgot my wallet."
 
 
 def test_combine_sentences_with_preposition():
@@ -27,4 +36,31 @@ def test_combine_sentences_with_preposition():
 
     combined_sentence = combine_sentences(sentence1, sentence2)
 
-    assert combined_sentence == "I studied for the test. The test was very difficult."
+    assert combined_sentence == "I studied for the test, and the test was very difficult."
+
+
+def test_combine_sentences_with_same_word():
+    sentence1 = "The cat was black."
+    sentence2 = "Black cats are considered lucky."
+
+    combined_sentence = combine_sentences(sentence1, sentence2)
+
+    assert combined_sentence == "The cat was black, and black cats are considered lucky."
+
+
+def test_combine_sentences_capitalization():
+    sentence1 = "the sun is shining."
+    sentence2 = "the sky is blue."
+
+    combined_sentence = combine_sentences(sentence1, sentence2)
+
+    assert combined_sentence == "The sun is shining, and the sky is blue."
+
+
+def test_combine_sentences_with_two_sentences_same():
+    sentence1 = "The cat was black."
+    sentence2 = "The cat was sleeping."
+
+    combined_sentence = combine_sentences(sentence1, sentence2)
+
+    assert combined_sentence == "The cat was black, and the cat was sleeping."
