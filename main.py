1737f0d0f404fc8beb1725c1657453ffca5c6fbb
Mark Avilin <46706090+MarkAvilin1@users.noreply.github.com>
2023-04-28 04:49:20 +0800

    Update main

--- main.py
+++ main.py
@@ -23,23 +23,22 @@ sorted_sentences = sort_sentences_by_info_density(text)
 print(sorted_sentences)
 # The answer is 'The quick brown fox jumps over the lazy dog.', 'The dog barks at the fox.', 'The cat sleeps on the mat.
 
+
 # Some Examples for task 3
 # Example 1
 sentence1 = "The cat sat on the mat."
 sentence2 = "The mat was soft and comfortable."
-
 combined_sentence = combine_sentences(sentence1, sentence2)
-
 print(combined_sentence)
-# The answer is The cat sat on the mat. The mat was soft and comfortable.
-
 
 # Example 2
 sentence1 = "I went to the store to buy some milk."
 sentence2 = "I also needed to get some bread."
-
 combined_sentence = combine_sentences(sentence1, sentence2)
-
 print(combined_sentence)
-# The answer is I went to the store to buy some milk. I also needed to get some bread.
 
+# Example 3
+sentence1 = 'I went to the store.'
+sentence2 = 'I bought some milk and eggs.'
+merged_sentence = combine_sentences(sentence1, sentence2)
+print(merged_sentence)
